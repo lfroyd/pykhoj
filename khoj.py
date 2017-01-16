b79f468bece61919c89e1dae88fa2c93f7f44422
Lars Frøyd <lars.froyd@gmail.com>
2017-01-16 22:02:46 +0100

    Test commit from new PC

--- khoj.py
+++ khoj.py
@@ -6,6 +6,7 @@ def main():
     app = QApplication(sys.argv)
     # app.setOrganizationName("NTNU - Waterpower Laboratory / KU - Turbine Testing Lab")
     # app.setOrganizationDomain("https://gitlab.com/khoj/pykhoj")
+    # app.setOrganizationDomain("https://github.com/lfroyd/pykhoj")
     app.setApplicationName("Khoj 2016")
     #app.setWindowIcon(QIcon(":/icon.png"))
     window = MainWindow.Window()
